432742f03ef14fdfbbd00b9b9db73735e11f411d
Miguel Rosa <miguelrosa.programacao@gmail.com>
2022-03-21 21:24:15 +0000

    Movie Info App: created top list

--- movie_info_app/lib/screens/home/home_screen.dart
+++ movie_info_app/lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_info_app/components/my_app_bar.dart';
+import 'package:movie_info_app/screens/home/components/body.dart';
 import 'package:movie_info_app/settings/constants.dart';
 
 
@@ -11,6 +12,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: TopAppBar(appTitle: APP_TITLE),
+      body: Body(),
     );
   }
 
