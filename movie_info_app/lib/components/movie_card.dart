b798ea738ce4001246d76d32debd118522c8e02c
Miguel Rosa <miguelrosa.programacao@gmail.com>
2022-03-24 19:57:19 +0000

    Movie Info App: finish

--- movie_info_app/lib/components/movie_card.dart
+++ movie_info_app/lib/components/movie_card.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:movie_info_app/models/movie.dart';
@@ -17,46 +18,47 @@ class MovieCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: KDEFAULTPADDING),
-      child: InkWell(
-        onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => DetailsScreen(
-                  movie: movie),
-            )),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  boxShadow: [KDEFAULTSHADOW],
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(movie.poster),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: KDEFAULTPADDING / 2),
-              child: Text(movie.title,
-                style: Theme.of(context).textTheme.headline5?.copyWith(
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(ICON_STAR_FILL, height: 20),
-                const SizedBox(width: KDEFAULTPADDING / 2),
-                Text("${movie.rating}", style: Theme.of(context).textTheme.bodyText2),
-              ],
-            ),
-          ],
-        ),
+      child: OpenContainer(
+        closedElevation: 0,
+        openElevation: 0,
+        closedBuilder: (context, action) => buildMovieCard(context),
+        openBuilder: (context, action) => DetailsScreen(movie: movie),
       ),
     );
+  }
+
+  Column buildMovieCard(BuildContext context) {
+    return Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [KDEFAULTSHADOW],
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(movie.poster),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: KDEFAULTPADDING / 2),
+            child: Text(movie.title,
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(ICON_STAR_FILL, height: 20),
+              const SizedBox(width: KDEFAULTPADDING / 2),
+              Text("${movie.rating}", style: Theme.of(context).textTheme.bodyText2),
+            ],
+          ),
+        ],
+      );
   }
 }
--- movie_info_app/lib/screens/details/components/movie_cast_crew.dart
+++ movie_info_app/lib/screens/details/components/movie_cast_crew.dart
@@ -13,31 +13,34 @@ class CrewAndCast extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(KDEFAULTPADDING),
-      child: Column(
-        children: [
-          Text("Cast & Crew", style: Theme.of(context).textTheme.headline5),
-          const SizedBox(height: KDEFAULTPADDING),
-          SizedBox(
-            height: 160,
-            child: ListView.builder(
-              itemCount: cast.length,
-              itemBuilder: (context, index) => CastCard(cast: cast[index]),
-              scrollDirection: Axis.horizontal,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Text("Cast & Crew", style: Theme.of(context).textTheme.headline5),
+            const SizedBox(height: KDEFAULTPADDING),
+            SizedBox(
+              height: 160,
+              child: ListView.builder(
+                itemCount: cast.length,
+                itemBuilder: (context, index) => CastCard(cast: cast[index]),
+                scrollDirection: Axis.horizontal,
+              ),
             ),
-          ),
-          const SizedBox(height: KDEFAULTPADDING / 2),
-          Text(cast['orginalName'],
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyText2,
-            maxLines: 2,
-          ),
-          const SizedBox(height: KDEFAULTPADDING / 4),
-          Text(cast['movieName'],
-            maxLines: 2,
-            textAlign: TextAlign.center,
-            style: const TextStyle(color: KTEXTLIGHTCOLOR),
-          ),
-        ],
+            const SizedBox(height: KDEFAULTPADDING / 2),
+            Text(cast['orginalName'],
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText2,
+              maxLines: 2,
+            ),
+            const SizedBox(height: KDEFAULTPADDING / 4),
+            Text(cast['movieName'],
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: KTEXTLIGHTCOLOR),
+            ),
+          ],
+        ),
       ),
     );
   }
