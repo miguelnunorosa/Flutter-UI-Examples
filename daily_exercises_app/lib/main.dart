d0af68f079e6b95598c3dca426bf0ec83575b4f7
Miguel Rosa <miguelrosa.programacao@gmail.com>
2022-04-04 11:41:04 +0100

    Exercises app: header

--- daily_exercises_app/lib/main.dart
+++ daily_exercises_app/lib/main.dart
@@ -1,51 +1,72 @@
 import 'package:daily_exercises_app/settings/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: "Cairo",
-          scaffoldBackgroundColor: KBACKGROUNDCOLOR,
-          textTheme: Theme.of(context).textTheme.apply(displayColor: KTEXTCOLOR),
-        ),
-      home: const HomeScreen()
+      title: 'Meditation App',
+      theme: ThemeData(
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: KBACKGROUNDCOLOR,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: KTEXTCOLOR),
+      ),
+      home: const HomeScreen(),
     );
   }
 }
-
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    var _screenSize = MediaQuery.of(context).size; //get screen (height and width) size
-
+    var _size = MediaQuery.of(context).size; //get screen size (height and width)
     return Scaffold(
       body: Stack(
-        children: [
+        children: <Widget>[
           Container(
-            height: _screenSize.height * .45, //container height is 45% of total screen size
+            height: _size.height * .45, //45% of total screen height size
             decoration: const BoxDecoration(
               color: KMAINHEADERCOLOR,
               image: DecorationImage(
                 alignment: Alignment.centerLeft,
-                image: AssetImage(IMAGE_CLOUD),
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
               ),
             ),
           ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: const BoxDecoration(
+                        color: KHEADERMENUICON,
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text("Good Morning \nMike!",
+                    style: Theme.of(context).textTheme.headline4?.copyWith(fontWeight: FontWeight.w900),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
--- daily_exercises_app/lib/settings/constants.dart
+++ daily_exercises_app/lib/settings/constants.dart
@@ -8,8 +8,4 @@ const KBLUELIGHTCOLOR = Color(0xFFC7B8F5);
 const KBLUECOLOR = Color(0xFF817DC0);
 const KSHADOWCOLOR = Color(0xFFE6E6E6);
 const KMAINHEADERCOLOR = Color(0xFFF5CEB8);
-
-
-//images
-const String IMAGE_MEDITATION_BG = "assets/images/meditation_bg.png";
-const String IMAGE_CLOUD = "assets/images/undraw_pilates_gpdb.png";
+const KHEADERMENUICON = Color(0xFFF2BEA1);
