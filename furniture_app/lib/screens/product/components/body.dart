9d0fe258011f198f23cdd77e7037580998331777
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-08-16 22:25:08 +0100

    furniture_app: Add search box widget

---
+++ furniture_app/lib/screens/product/components/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_app/components/search_box.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchBox(onChanged: (value) {},);
+  }
+}
+
